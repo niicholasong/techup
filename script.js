322608bad9c52e342738733d728d8dc469c13bd2
niicholasong <60722322+niicholasong@users.noreply.github.com>
2025-05-07 06:55:41 +0000

    Changes

--- script.js
+++ script.js
@@ -36,3 +36,14 @@ function sayHello() {
     alert("Hello everyone");
 }
 
+function showAge() {
+
+    // Get the value from the input field of ID #user-input
+    // Store this value in a variable called user_input
+    var user_input = document.getElementById("user-input").value
+
+    // Update the content for the HTML element with ID #header-age with 
+    // whatever the user_input varible contains
+    document.getElementById("header-age").textContent = user_input
+}
+
